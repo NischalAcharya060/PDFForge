4a287e066f7c19c4f40e7faa773d03568b9256e1
Nischal <Nischal060@gmail.com>
2026-09-25 09:04:58 +0545

    download fixes

--- next.config.ts
+++ next.config.ts
@@ -13,13 +13,19 @@ const nextConfig: NextConfig = {
       {
         source: "/PDFForge-Setup.exe",
         destination:
-          "https://github.com/NischalAcharya060/PDFForge-Viewer/releases/download/v1.0.0/PDFForge-Setup.exe",
+          "https://github.com/NischalAcharya060/PDFForge-Viewer/releases/download/v1.1.0/PDFForge-Setup.exe",
         permanent: false,
       },
       {
         source: "/releases/1.0.0/PDFForge-Setup.exe",
         destination:
           "https://github.com/NischalAcharya060/PDFForge-Viewer/releases/download/v1.0.0/PDFForge-Setup.exe",
+        permanent: false,
+      },
+      {
+        source: "/releases/1.1.0/PDFForge-Setup.exe",
+        destination:
+          "https://github.com/NischalAcharya060/PDFForge-Viewer/releases/download/v1.1.0/PDFForge-Setup.exe",
         permanent: false,
       },
     ];
